b37e53b108e7f816a7cfec3a5c34621e9d0c14ee
rasyiffadila <41820010116@student.mercubuana.ac.id>
2021-12-23 22:10:45 +0700

    Menambahkan file

---
+++ app/src/main/java/com/example/courselist/Database/DatabaseQueryClass.java
@@ -0,0 +1,402 @@
+package com.example.courselist.Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+//import android.util.Config;
+import android.widget.Toast;
+import com.example.courselist.Util.Config;
+import com.example.courselist.Features.CreateStudent.Student;
+import com.example.courselist.Features.CreateSubject.Subject;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DatabaseQueryClass {
+    private Context context;
+
+    public DatabaseQueryClass(Context context){
+        this.context = context;
+        Logger.addLogAdapter(new AndroidLogAdapter());
+    }
+
+    public long insertStudent(Student student){
+
+        long id = -1;
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Config.COLUMN_STUDENT_NAME, student.getName());
+        contentValues.put(Config.COLUMN_STUDENT_REGISTRATION, student.getRegistrationNumber());
+        contentValues.put(Config.COLUMN_STUDENT_PHONE, student.getPhoneNumber());
+        contentValues.put(Config.COLUMN_STUDENT_EMAIL, student.getEmail());
+
+        try {
+            id = sqLiteDatabase.insertOrThrow(Config.TABLE_STUDENT, null, contentValues);
+        } catch (SQLiteException e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, "Operation failed: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            sqLiteDatabase.close();
+        }
+
+        return id;
+    }
+
+    public List<Student> getAllStudent(){
+
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getReadableDatabase();
+
+        Cursor cursor = null;
+        try {
+
+            cursor = sqLiteDatabase.query(Config.TABLE_STUDENT, null, null, null, null, null, null, null);
+
+            /**
+             // If you want to execute raw query then uncomment below 2 lines. And comment out above line.
+
+             String SELECT_QUERY = String.format("SELECT %s, %s, %s, %s, %s FROM %s", Config.COLUMN_STUDENT_ID, Config.COLUMN_STUDENT_NAME, Config.COLUMN_STUDENT_REGISTRATION, Config.COLUMN_STUDENT_EMAIL, Config.COLUMN_STUDENT_PHONE, Config.TABLE_STUDENT);
+             cursor = sqLiteDatabase.rawQuery(SELECT_QUERY, null);
+             */
+
+            if(cursor!=null)
+                if(cursor.moveToFirst()){
+                    List<Student> studentList = new ArrayList<>();
+                    do {
+                        int id = cursor.getInt(cursor.getColumnIndex(Config.COLUMN_STUDENT_ID));
+                        String name = cursor.getString(cursor.getColumnIndex(Config.COLUMN_STUDENT_NAME));
+                        long registrationNumber = cursor.getLong(cursor.getColumnIndex(Config.COLUMN_STUDENT_REGISTRATION));
+                        String email = cursor.getString(cursor.getColumnIndex(Config.COLUMN_STUDENT_EMAIL));
+                        String phone = cursor.getString(cursor.getColumnIndex(Config.COLUMN_STUDENT_PHONE));
+
+                        studentList.add(new Student(id, name, registrationNumber, phone, email));
+                    }   while (cursor.moveToNext());
+
+                    return studentList;
+                }
+        } catch (Exception e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, "Operation failed", Toast.LENGTH_SHORT).show();
+        } finally {
+            if(cursor!=null)
+                cursor.close();
+            sqLiteDatabase.close();
+        }
+
+        return Collections.emptyList();
+    }
+
+    public Student getStudentByRegNum(long registrationNum){
+
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getReadableDatabase();
+
+        Cursor cursor = null;
+        Student student = null;
+        try {
+
+            cursor = sqLiteDatabase.query(Config.TABLE_STUDENT, null,
+                    Config.COLUMN_STUDENT_REGISTRATION + " = ? ", new String[]{String.valueOf(registrationNum)},
+                    null, null, null);
+
+            /**
+             // If you want to execute raw query then uncomment below 2 lines. And comment out above sqLiteDatabase.query() method.
+
+             String SELECT_QUERY = String.format("SELECT * FROM %s WHERE %s = %s", Config.TABLE_STUDENT, Config.COLUMN_STUDENT_REGISTRATION, String.valueOf(registrationNum));
+             cursor = sqLiteDatabase.rawQuery(SELECT_QUERY, null);
+             */
+
+            if(cursor.moveToFirst()){
+                int id = cursor.getInt(cursor.getColumnIndex(Config.COLUMN_STUDENT_ID));
+                String name = cursor.getString(cursor.getColumnIndex(Config.COLUMN_STUDENT_NAME));
+                long registrationNumber = cursor.getLong(cursor.getColumnIndex(Config.COLUMN_STUDENT_REGISTRATION));
+                String phone = cursor.getString(cursor.getColumnIndex(Config.COLUMN_STUDENT_PHONE));
+                String email = cursor.getString(cursor.getColumnIndex(Config.COLUMN_STUDENT_EMAIL));
+
+                student = new Student(id, name, registrationNumber, phone, email);
+            }
+        } catch (Exception e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, "Operation failed", Toast.LENGTH_SHORT).show();
+        } finally {
+            if(cursor!=null)
+                cursor.close();
+            sqLiteDatabase.close();
+        }
+
+        return student;
+    }
+
+    public long updateStudentInfo(Student student){
+
+        long rowCount = 0;
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Config.COLUMN_STUDENT_NAME, student.getName());
+        contentValues.put(Config.COLUMN_STUDENT_REGISTRATION, student.getRegistrationNumber());
+        contentValues.put(Config.COLUMN_STUDENT_PHONE, student.getPhoneNumber());
+        contentValues.put(Config.COLUMN_STUDENT_EMAIL, student.getEmail());
+
+        try {
+            rowCount = sqLiteDatabase.update(Config.TABLE_STUDENT, contentValues,
+                    Config.COLUMN_STUDENT_ID + " = ? ",
+                    new String[] {String.valueOf(student.getId())});
+        } catch (SQLiteException e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            sqLiteDatabase.close();
+        }
+
+        return rowCount;
+    }
+
+    public long deleteStudentByRegNum(long registrationNum) {
+        long deletedRowCount = -1;
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        try {
+            deletedRowCount = sqLiteDatabase.delete(Config.TABLE_STUDENT,
+                    Config.COLUMN_STUDENT_REGISTRATION + " = ? ",
+                    new String[]{ String.valueOf(registrationNum)});
+        } catch (SQLiteException e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            sqLiteDatabase.close();
+        }
+
+        return deletedRowCount;
+    }
+
+    public boolean deleteAllStudents(){
+        boolean deleteStatus = false;
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        try {
+            //for "1" delete() method returns number of deleted rows
+            //if you don't want row count just use delete(TABLE_NAME, null, null)
+            sqLiteDatabase.delete(Config.TABLE_STUDENT, null, null);
+
+            long count = DatabaseUtils.queryNumEntries(sqLiteDatabase, Config.TABLE_STUDENT);
+
+            if(count==0)
+                deleteStatus = true;
+
+        } catch (SQLiteException e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            sqLiteDatabase.close();
+        }
+
+        return deleteStatus;
+    }
+
+
+
+//    SUBJECT
+
+    public long insertSubject(Subject subject){
+
+        long id = -1;
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Config.COLUMN_SUBJECT_NAME, subject.getSubject_name());
+        contentValues.put(Config.COLUMN_SUBJECT_CODE, subject.getSubject_code());
+        contentValues.put(Config.COLUMN_SUBJECT_CREDIT, subject.getSubject_credit());
+
+        try {
+            id = sqLiteDatabase.insertOrThrow(Config.TABLE_SUBJECT, null, contentValues);
+        } catch (SQLiteException e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, "Operation failed: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            sqLiteDatabase.close();
+        }
+
+        return id;
+    }
+
+    public List<Subject> getAllSubject(){
+
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getReadableDatabase();
+
+        Cursor cursor = null;
+        try {
+
+            cursor = sqLiteDatabase.query(Config.TABLE_SUBJECT, null, null, null, null, null, null, null);
+
+            /**
+             // If you want to execute raw query then uncomment below 2 lines. And comment out above line.
+
+             String SELECT_QUERY = String.format("SELECT %s, %s, %s, %s, %s FROM %s", Config.COLUMN_STUDENT_ID, Config.COLUMN_STUDENT_NAME, Config.COLUMN_STUDENT_REGISTRATION, Config.COLUMN_STUDENT_EMAIL, Config.COLUMN_STUDENT_PHONE, Config.TABLE_STUDENT);
+             cursor = sqLiteDatabase.rawQuery(SELECT_QUERY, null);
+             */
+
+            if(cursor!=null)
+                if(cursor.moveToFirst()){
+                    List<Subject> subjectList = new ArrayList<>();
+                    do {
+                        int id = cursor.getInt(cursor.getColumnIndex(Config.COLUMN_SUBJECT_ID));
+                        String subject_name = cursor.getString(cursor.getColumnIndex(Config.COLUMN_SUBJECT_NAME));
+                        long subject_code = cursor.getLong(cursor.getColumnIndex(Config.COLUMN_SUBJECT_CODE));
+                        long subject_credit = cursor.getLong(cursor.getColumnIndex(Config.COLUMN_SUBJECT_CREDIT));
+                        subjectList.add(new Subject(id, subject_name, subject_code, subject_credit));
+                    }   while (cursor.moveToNext());
+
+                    return subjectList;
+                }
+        } catch (Exception e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, "Operation failed", Toast.LENGTH_SHORT).show();
+        } finally {
+            if(cursor!=null)
+                cursor.close();
+            sqLiteDatabase.close();
+        }
+
+        return Collections.emptyList();
+    }
+
+    public Subject getSubjectByCode(long code){
+
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getReadableDatabase();
+
+        Cursor cursor = null;
+        Subject subject = null;
+        try {
+
+            cursor = sqLiteDatabase.query(Config.TABLE_SUBJECT, null,
+                    Config.COLUMN_SUBJECT_CODE + " = ? ", new String[]{String.valueOf(code)},
+                    null, null, null);
+
+            /**
+             // If you want to execute raw query then uncomment below 2 lines. And comment out above sqLiteDatabase.query() method.
+
+             String SELECT_QUERY = String.format("SELECT * FROM %s WHERE %s = %s", Config.TABLE_STUDENT, Config.COLUMN_STUDENT_REGISTRATION, String.valueOf(registrationNum));
+             cursor = sqLiteDatabase.rawQuery(SELECT_QUERY, null);
+             */
+
+            if(cursor.moveToFirst()){
+                int id = cursor.getInt(cursor.getColumnIndex(Config.COLUMN_SUBJECT_ID));
+                String subject_name = cursor.getString(cursor.getColumnIndex(Config.COLUMN_SUBJECT_NAME));
+                long subject_code = cursor.getLong(cursor.getColumnIndex(Config.COLUMN_SUBJECT_CODE));
+                long subject_credit = cursor.getLong(cursor.getColumnIndex(Config.COLUMN_SUBJECT_CREDIT));
+
+                subject = new Subject(id, subject_name, subject_code, subject_credit);
+            }
+        } catch (Exception e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, "Operation failed", Toast.LENGTH_SHORT).show();
+        } finally {
+            if(cursor!=null)
+                cursor.close();
+            sqLiteDatabase.close();
+        }
+
+        return subject;
+    }
+    public long updateSubjectInfo(Subject subject){
+
+        long rowCount = 0;
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Config.COLUMN_SUBJECT_NAME, subject.getSubject_name());
+        contentValues.put(Config.COLUMN_SUBJECT_CODE, subject.getSubject_code());
+        contentValues.put(Config.COLUMN_SUBJECT_CREDIT, subject.getSubject_credit());
+
+        try {
+            rowCount = sqLiteDatabase.update(Config.TABLE_SUBJECT, contentValues,
+                    Config.COLUMN_SUBJECT_ID + " = ? ",
+                    new String[] {String.valueOf(subject.getId())});
+        } catch (SQLiteException e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            sqLiteDatabase.close();
+        }
+
+        return rowCount;
+    }
+
+    public long deleteSubjectByCode(long code) {
+        long deletedRowCount = -1;
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        try {
+            deletedRowCount = sqLiteDatabase.delete(Config.TABLE_SUBJECT,
+                    Config.COLUMN_SUBJECT_CODE + " = ? ",
+                    new String[]{ String.valueOf(code)});
+        } catch (SQLiteException e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            sqLiteDatabase.close();
+        }
+
+        return deletedRowCount;
+    }
+
+    public boolean deleteAllSubjects(){
+        boolean deleteStatus = false;
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        try {
+            //for "1" delete() method returns number of deleted rows
+            //if you don't want row count just use delete(TABLE_NAME, null, null)
+            sqLiteDatabase.delete(Config.TABLE_SUBJECT, null, null);
+
+            long count = DatabaseUtils.queryNumEntries(sqLiteDatabase, Config.TABLE_SUBJECT);
+
+            if(count==0)
+                deleteStatus = true;
+
+        } catch (SQLiteException e){
+            Logger.d("Exception: " + e.getMessage());
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            sqLiteDatabase.close();
+        }
+
+        return deleteStatus;
+    }
+
+    public int studentCount(){
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT COUNT(*) FROM student", null);
+        cursor.moveToFirst();
+        int count = cursor.getInt(0);
+        return count;
+    }
+
+    public int subjectCount(){
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT COUNT(*) FROM subject", null);
+        cursor.moveToFirst();
+        int count = cursor.getInt(0);
+        return count;
+    }
+
+}
---
+++ app/src/main/java/com/example/courselist/Util/Config.java
@@ -0,0 +1,35 @@
+package com.example.courselist.Util;
+
+public class Config {
+    public static final String DATABASE_NAME = "student-db";
+
+    //column names of student table
+    public static final String TABLE_STUDENT = "student";
+    public static final String TABLE_SUBJECT = "subject";
+
+    public static final String COLUMN_STUDENT_ID = "_id";
+    public static final String COLUMN_STUDENT_NAME = "name";
+    public static final String COLUMN_STUDENT_REGISTRATION = "registration_no";
+    public static final String COLUMN_STUDENT_PHONE = "phone";
+    public static final String COLUMN_STUDENT_EMAIL = "email";
+
+
+    public static final String COLUMN_SUBJECT_ID = "_id";
+    public static final String COLUMN_SUBJECT_NAME = "subject_name";
+    public static final String COLUMN_SUBJECT_CODE = "subject_code";
+    public static final String COLUMN_SUBJECT_CREDIT = "subject_credit";
+
+    //others for general purpose key-value pair data
+    public static final String TITLE = "title";
+    public static final String CREATE_STUDENT = "create_student";
+    public static final String UPDATE_STUDENT = "update_student";
+
+    public static final String CREATE_SUBJECT = "create_subject";
+    public static final String UPDATE_SUBJECT = "update_subject";
+
+    //column names of student table
+
+
+    //others for general purpose key-value pair data
+
+}
